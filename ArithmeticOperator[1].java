5124888907449f1b4a3e152aeab75cd1d73155ab
ThriveniBujji-143 <126540634+ThriveniBujji-143@users.noreply.github.com>
2023-12-06 21:54:32 +0530

    Add files via upload

---
+++ ArithmeticOperator[1].java
@@ -0,0 +1,17 @@
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int a=20,b=5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println("yash"+"Sai");
+        System.out.println(10+"yash");
+        System.out.println(10+"yash"+20+30);
+        System.out.println(10+20+"yash"+20+30);
+        System.out.println(10+"yash"+3*10/2+"Sai");
+        /*System.out.println(10+"yash"+3*10/2-1+"Sai");
+        - is not possible for string*/
+    }    
+}
